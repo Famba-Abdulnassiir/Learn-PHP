48b0689fdf0d840833c3e5473e70f9f63cd297fb
Famba Abdulnassiir Siraji <57795345+FambaAbdulnassiirSiraji@users.noreply.github.com>
2024-04-21 16:04:46 +0300

    add files, notes on vairables

---
+++ Skills_share_php_101/helloworld.php
@@ -0,0 +1,18 @@
+<?php
+$var = 'Bob';
+$Var = 'Joe';
+echo "$var, $Var";      // outputs "Bob, Joe"
+
+//$4site = 'not yet';     // invalid; starts with a number
+
+$_4site = 'not yet';    // valid; starts with an underscore
+$täyte = 'mansikka';    // valid; 'ä' is (Extended) ASCII 228.
+
+$a = 1;
+$b = 2;
+$float = 1.2;
+$isUser = true; //boolean
+
+//it will automatically pick the data type by reading it as defined on the variable.
+?>
+ 
